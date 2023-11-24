4d293230de106433fbe88b0e858a32365fa7de44
nikirana22 <nikirana1983@gmail.com>
2023-11-24 20:12:02 +0530

    feat: implemented persistent_bottom_nav_bar lib.

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
+import 'package:persistent_bottom_nav_bar/persistent_tab_view.dart';
 import '/screen/product_screen.dart';
-import '/provider/home_provider.dart';
 import '/screen/cart_screen.dart';
 import '/screen/checkout_screen.dart';
 
@@ -16,24 +15,27 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    int index =
-        context.select<HomeProvider, int>((value) => value.currentPageIndex);
-
-    return Scaffold(
-      body: SafeArea(child: _screens[index]),
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: index,
-        selectedItemColor: Colors.blue,
-        unselectedItemColor: Colors.black,
-        onTap: (index) => context.read<HomeProvider>().updateIndex(index),
-        items: const [
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.add_shopping_cart_outlined), label: 'Cart'),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.check_outlined), label: 'Checkout')
-        ],
-      ),
+    return PersistentTabView(
+      context,
+      screens: _screens,
+      items: _buildItems(),
     );
+  }
+
+  List<PersistentBottomNavBarItem> _buildItems() {
+    return [
+      PersistentBottomNavBarItem(
+          inactiveColorPrimary: Colors.grey,
+          icon: const Icon(Icons.home),
+          title: 'Home'),
+      PersistentBottomNavBarItem(
+          inactiveColorPrimary: Colors.grey,
+          icon: const Icon(Icons.add_shopping_cart_outlined),
+          title: 'Cart'),
+      PersistentBottomNavBarItem(
+          inactiveColorPrimary: Colors.grey,
+          icon: const Icon(Icons.check_outlined),
+          title: 'Checkout'),
+    ];
   }
 }
